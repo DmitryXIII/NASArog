a61d001aee3453ffbd3465e423d6f4ef74270df8
Dmitry Gusev <dmtrxiii@gmail.com>
2022-03-27 01:09:10 +0300

    task 5 released (space animation added)

--- app/src/main/java/com/ineedyourcode/nasarog/view/ui/features/FeaturesListFragment.kt
+++ app/src/main/java/com/ineedyourcode/nasarog/view/ui/features/FeaturesListFragment.kt
@@ -2,14 +2,14 @@ package com.ineedyourcode.nasarog.view.ui.features
 
 import android.os.Bundle
 import android.view.View
-import android.view.animation.AnticipateOvershootInterpolator
-import android.view.animation.BounceInterpolator
-import android.view.animation.OvershootInterpolator
+import android.view.animation.*
+import android.widget.FrameLayout
 import androidx.interpolator.view.animation.LinearOutSlowInInterpolator
 import androidx.navigation.fragment.findNavController
 import com.ineedyourcode.nasarog.R
 import com.ineedyourcode.nasarog.databinding.FragmentFeaturesListBinding
 import com.ineedyourcode.nasarog.view.basefragment.BaseFragment
+import kotlin.random.Random
 
 /**
  * MultiBackstack из navigation component создает проблемы при воспроизведении анимации.
@@ -17,9 +17,13 @@ import com.ineedyourcode.nasarog.view.basefragment.BaseFragment
  * обнуляется все, что связано с координатами на экране, из-за этого анимация начинает вести себя неправильно.
  * Долго пытался поймать в жизненных циклах фрагмента разные состояния savedInstanceState и
  * findNavController().currentBackStackEntry?.savedStateHandle?.getLiveData, но так и не уловил момент, почему
- * происходит обнуление координат.
+ * происходит обнуление значений координат виджетов.
  * Оставлю себе это как факультатив на каникулах.
- * Сейчас анимация отображается только при переходе на этот фрагмент через bottomNavigationView.
+ * Сейчас анимация отображается корректно только при переходе на этот фрагмент через bottomNavigationView.
+ * ===============
+ * ===============
+ * Обе анимации (передвигающиеся круги и звездное небо) зациклены, из-за большого количества звезд на небе
+ * на эмуляторе может подтормаживать, на реальном устройстве смотрится интереснее.
  */
 
 class FeaturesListFragment :
@@ -29,7 +33,11 @@ class FeaturesListFragment :
         const val KEY_BACK_STACK_ENTRY = "KEY_BACK_STACK_ENTRY"
     }
 
-    private val viewList = mutableListOf<View>()
+    private val starsList = mutableListOf<View>()
+
+    private val starsColorsList = mutableListOf<Int>()
+
+    private val dotsList = mutableListOf<View>()
 
     private var savedInstance: Bundle? = null
 
@@ -37,6 +45,8 @@ class FeaturesListFragment :
 
     private var rangeX = 0f
     private var rangeY = 0f
+    private var spaceWidth = 0f
+    private var spaceHeight = 0f
 
     private var dotScale = 1f
 
@@ -62,6 +72,8 @@ class FeaturesListFragment :
             with(binding) {
                 rangeX = rootContainer.width.toFloat() - animatedCircle1.width
                 rangeY = rootContainer.height.toFloat() - animatedCircle1.width
+                spaceWidth = rootContainer.height.toFloat()
+                spaceHeight = rootContainer.height.toFloat()
 
                 dotStartMovingDelay = animatedCircle1.width.toLong()
 
@@ -77,16 +89,25 @@ class FeaturesListFragment :
                 scene4DestinationY = rootContainer.y
                 scene5DestinationX = rootContainer.x
 
-                viewList.add(animatedCircle1)
-                viewList.add(animatedCircle2)
-                viewList.add(animatedCircle3)
-                viewList.add(animatedCircle4)
-                viewList.add(animatedCircle5)
+                dotsList.add(animatedCircle1)
+                dotsList.add(animatedCircle2)
+                dotsList.add(animatedCircle3)
+                dotsList.add(animatedCircle4)
+                dotsList.add(animatedCircle5)
             }
             backEntry = false
-            animationScene1FallDown(viewList)
-        }
 
+            starsColorsList.add(R.drawable.space_star_color_1)
+            starsColorsList.add(R.drawable.space_star_color_2)
+            starsColorsList.add(R.drawable.space_star_color_3)
+            starsColorsList.add(R.drawable.space_star_color_4)
+            starsColorsList.add(R.drawable.space_star_color_5)
+
+            dotsAnimationScene1FallDown(dotsList)
+            for (starView in starsList) {
+                animationSpaceBackgroundScene1(starView)
+            }
+        }
         super.onResume()
     }
 
@@ -94,6 +115,13 @@ class FeaturesListFragment :
         super.onViewCreated(view, savedInstanceState)
 
         savedInstance = savedInstanceState
+
+        for (i in 0..299) {
+            starsList.add(View(requireContext()).apply {
+                alpha = 0f
+            })
+            binding.rootFrame.addView(starsList[i])
+        }
 
         backEntry =
             findNavController().currentBackStackEntry?.savedStateHandle?.getLiveData<Boolean>(
@@ -114,7 +142,7 @@ class FeaturesListFragment :
         }
     }
 
-    private fun animationScene1FallDown(viewList: List<View>) {
+    private fun dotsAnimationScene1FallDown(viewList: List<View>) {
         delay = 300
         for (view in viewList) {
             view.animate()
@@ -124,7 +152,7 @@ class FeaturesListFragment :
                 .setInterpolator(BounceInterpolator())
                 .withEndAction {
                     when (view.id) {
-                        R.id.animated_circle_5 -> animationScene2MoveInOneDot(viewList)
+                        R.id.animated_circle_5 -> dotsAnimationScene2MoveInOneDot(viewList)
                     }
                 }
                 .duration = 1500
@@ -133,7 +161,7 @@ class FeaturesListFragment :
         delay = 0
     }
 
-    private fun animationScene2MoveInOneDot(viewList: List<View>) {
+    private fun dotsAnimationScene2MoveInOneDot(viewList: List<View>) {
         for (view in viewList) {
             view.animate()
                 .x(dot3startX)
@@ -149,14 +177,14 @@ class FeaturesListFragment :
                 .setInterpolator(AnticipateOvershootInterpolator())
                 .withEndAction {
                     when (view.id) {
-                        R.id.animated_circle_5 -> animationScene3MoveToBottomRight(viewList)
+                        R.id.animated_circle_5 -> dotsAnimationScene3MoveToBottomRight(viewList)
                     }
                 }
                 .duration = 1000
         }
     }
 
-    private fun animationScene3MoveToBottomRight(viewList: List<View>) {
+    private fun dotsAnimationScene3MoveToBottomRight(viewList: List<View>) {
         delay = 150
         for (view in viewList) {
             view.animate()
@@ -167,7 +195,7 @@ class FeaturesListFragment :
                 .setInterpolator(LinearOutSlowInInterpolator())
                 .withEndAction {
                     when (view.id) {
-                        R.id.animated_circle_5 -> animationScene4MoveToTopRight(viewList)
+                        R.id.animated_circle_5 -> dotsAnimationScene4MoveToTopRight(viewList)
                     }
                 }
                 .duration = (rangeX).toLong()
@@ -178,7 +206,7 @@ class FeaturesListFragment :
         dotScale = 1f
     }
 
-    private fun animationScene4MoveToTopRight(viewList: List<View>) {
+    private fun dotsAnimationScene4MoveToTopRight(viewList: List<View>) {
         for (view in viewList) {
             view.animate()
                 .y(scene4DestinationY)
@@ -186,7 +214,7 @@ class FeaturesListFragment :
                 .setInterpolator(LinearOutSlowInInterpolator())
                 .withEndAction {
                     when (view.id) {
-                        R.id.animated_circle_5 -> animationScene5MoveToTopLeft(viewList)
+                        R.id.animated_circle_5 -> dotsAnimationScene5MoveToTopLeft(viewList)
                     }
                 }
                 .duration = (rangeY / dotSpeed).toLong()
@@ -195,7 +223,7 @@ class FeaturesListFragment :
         delay = 0
     }
 
-    private fun animationScene5MoveToTopLeft(viewList: List<View>) {
+    private fun dotsAnimationScene5MoveToTopLeft(viewList: List<View>) {
         for (view in viewList) {
             view.animate()
                 .x(scene5DestinationX)
@@ -203,7 +231,7 @@ class FeaturesListFragment :
                 .setInterpolator(LinearOutSlowInInterpolator())
                 .withEndAction {
                     when (view.id) {
-                        R.id.animated_circle_5 -> animationScene6MoveToBottomLeft(viewList)
+                        R.id.animated_circle_5 -> dotsAnimationScene6MoveToBottomLeft(viewList)
                     }
                 }
                 .duration = (rangeX).toLong()
@@ -212,7 +240,7 @@ class FeaturesListFragment :
         delay = 0
     }
 
-    private fun animationScene6MoveToBottomLeft(viewList: List<View>) {
+    private fun dotsAnimationScene6MoveToBottomLeft(viewList: List<View>) {
         for (view in viewList) {
             view.animate()
                 .y(scene1DestinationY)
@@ -220,7 +248,7 @@ class FeaturesListFragment :
                 .setInterpolator(LinearOutSlowInInterpolator())
                 .withEndAction {
                     when (view.id) {
-                        R.id.animated_circle_5 -> animationScene7MoveToOneDot(viewList)
+                        R.id.animated_circle_5 -> dotsAnimationScene7MoveToOneDot(viewList)
                     }
                 }
                 .duration = (rangeY / dotSpeed).toLong()
@@ -229,7 +257,7 @@ class FeaturesListFragment :
         delay = 0
     }
 
-    private fun animationScene7MoveToOneDot(viewList: List<View>) {
+    private fun dotsAnimationScene7MoveToOneDot(viewList: List<View>) {
         for (view in viewList) {
             view.animate()
                 .scaleX(1f)
@@ -239,7 +267,7 @@ class FeaturesListFragment :
                 .setInterpolator(OvershootInterpolator())
                 .withEndAction {
                     when (view.id) {
-                        R.id.animated_circle_5 -> animationScene8MoveToFiveDots(viewList)
+                        R.id.animated_circle_5 -> dotsAnimationScene8MoveToFiveDots(viewList)
                     }
                 }
                 .duration = (rangeY / dotSpeed * 1.5).toLong()
@@ -248,7 +276,7 @@ class FeaturesListFragment :
         delay = 0
     }
 
-    private fun animationScene8MoveToFiveDots(viewList: List<View>) {
+    private fun dotsAnimationScene8MoveToFiveDots(viewList: List<View>) {
         for (view in viewList) {
             view.animate()
                 .x(
@@ -273,7 +301,7 @@ class FeaturesListFragment :
                 .withEndAction {
                     when (view.id) {
                         R.id.animated_circle_5 -> {
-                            animationScene9MoveToStartPositions(viewList)
+                            dotsAnimationScene9MoveToStartPositions(viewList)
                         }
                     }
                 }
@@ -281,7 +309,7 @@ class FeaturesListFragment :
         }
     }
 
-    private fun animationScene9MoveToStartPositions(viewList: List<View>) {
+    private fun dotsAnimationScene9MoveToStartPositions(viewList: List<View>) {
         for (view in viewList) {
             view.animate()
                 .alpha(0f)
@@ -290,12 +318,92 @@ class FeaturesListFragment :
                 .setInterpolator(AnticipateOvershootInterpolator())
                 .withEndAction {
                     when (view.id) {
-                        R.id.animated_circle_5 -> animationScene1FallDown(viewList)
+                        R.id.animated_circle_5 -> dotsAnimationScene1FallDown(viewList)
                     }
                 }
                 .duration = 1000
             delay += 150
         }
         delay = 0
+    }
+
+    private fun animationSpaceBackgroundScene1(starView: View) {
+        val radius = (1..10).random()
+        starView.apply {
+            setBackgroundResource(starsColorsList[Random.nextInt(starsColorsList.size)])
+            layoutParams = FrameLayout.LayoutParams(radius, radius)
+            x = (0..spaceWidth.toInt()).random().toFloat()
+            y = (0..spaceHeight.toInt()).random().toFloat()
+            animate()
+                .setStartDelay((100..10000).random().toLong())
+                .alpha(Random.nextFloat())
+                .setInterpolator(LinearInterpolator())
+                .withEndAction { animationSpaceBackgroundScene2(starView) }
+                .duration = (100..10000).random().toLong()
+        }
+    }
+
+    private fun animationSpaceBackgroundScene2(starView: View) {
+        starView.apply {
+            animate()
+                .setStartDelay((3000..10000).random().toLong())
+                .alpha(0f)
+                .setInterpolator(LinearInterpolator())
+                .withEndAction {
+                    animationSpaceBackgroundScene3(starView)
+                }
+                .duration = (100..2000).random().toLong()
+        }
+    }
+
+    private fun animationSpaceBackgroundScene3(starView: View) {
+        val radius = (1..10).random()
+        starView.apply {
+            setBackgroundResource(starsColorsList[Random.nextInt(starsColorsList.size)])
+            layoutParams = FrameLayout.LayoutParams(radius, radius)
+            x = (0..spaceWidth.toInt()).random().toFloat()
+            y = (0..spaceHeight.toInt()).random().toFloat()
+            when (Random.nextFloat()) {
+                in 0f..0.85f -> {
+                    animate()
+                        .setStartDelay((0..1000).random().toLong())
+                        .alpha(Random.nextFloat())
+                        .setInterpolator(LinearInterpolator())
+                        .withEndAction { animationSpaceBackgroundScene2(starView) }
+                        .duration = (300..10000).random().toLong()
+                }
+                else -> {
+                    alpha = 0f
+                    val flyingToX = if (Random.nextBoolean()) {
+                        (0..(spaceWidth).toInt()).random().toFloat()
+                    } else {
+                        ((0..(spaceWidth).toInt()).random().toFloat()) * -1
+                    }
+                    val flyingToY = if (Random.nextBoolean()) {
+                        (0..(spaceHeight).toInt()).random().toFloat()
+                    } else {
+                        ((0..(spaceHeight).toInt()).random().toFloat()) * -1
+                    }
+                    val flyingDuration = (200..1000).random().toLong()
+                    animate()
+                        .setStartDelay(0)
+                        .alpha(1f)
+                        .x(starView.x + flyingToX / 2)
+                        .y(starView.y + flyingToY / 2)
+                        .setInterpolator(LinearInterpolator())
+                        .withEndAction {
+                            animate()
+                                .setStartDelay(0)
+                                .alpha(0f)
+                                .x(starView.x + flyingToX / 2)
+                                .y(starView.y + flyingToY / 2)
+                                .setInterpolator(LinearInterpolator())
+                                .withEndAction { animationSpaceBackgroundScene3(starView) }
+                                .duration = flyingDuration
+                        }
+                        .duration = flyingDuration
+                }
+            }
+        }
     }
 }
